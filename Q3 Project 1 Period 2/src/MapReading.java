6d104013bdf10ce176759feb45e1d586f81983ab
1951951 <1951951@01-18134.pusd.dom>
2026-02-23 11:54:16 -0800

    loool

---
+++ Q3 Project 1 Period 2/src/MapReading.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MapReading {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		String[][][] map2 = readCoordinateMap(new File("coordinate.txt"));
+		
+		printMap(map2);
+	}
+	
+	public static String[][][] readMap(File file) throws FileNotFoundException{
+		Scanner scanner = new Scanner(file);
+		
+		int numRows = scanner.nextInt();
+		int numCols = scanner.nextInt();
+		int numLevels = scanner.nextInt();
+		
+		String[][][] map = new String[numLevels][numRows][numCols];
+		
+		for (int level = 0; level < map.length; level++) {
+			for (int row = 0; row < map[level].length; row++) {
+				String line = scanner.next();
+				for (int col = 0; col < map[level][row].length; col++) {
+					map[level][row][col] = line.substring(col, col+1);
+				}
+			}
+		}
+		
+		scanner.close();
+		
+		return map;
+	}
+	
+	public static String[][][] readCoordinateMap(File file) throws FileNotFoundException {
+		Scanner scanner = new Scanner(file);
+		
+		int numRows = scanner.nextInt();
+		int numCols = scanner.nextInt();
+		int numLevels = scanner.nextInt();
+		
+		String[][][] map = new String[numLevels][numRows][numCols];
+		
+		while (scanner.hasNext()) {
+			String symbol = scanner.next();
+			int row = Integer.parseInt(scanner.next());
+			int col = Integer.parseInt(scanner.next());
+			int level = Integer.parseInt(scanner.next());
+			
+			map[level][row][col] = symbol;
+		}
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				for (int k = 0; k < map[i][j].length; k++) {
+					if (map[i][j][k] == null) {
+						map[i][j][k] = ".";
+					}
+				}
+			}
+		}
+		scanner.close();
+		
+		return map;
+	}
+	
+	
+	public static void printMap(String[][][] map) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0 ; j < map[i].length; j++) {
+				for (int k = 0; k < map[i][j].length; k++) {
+					System.out.print(map[i][j][k]);
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+}
